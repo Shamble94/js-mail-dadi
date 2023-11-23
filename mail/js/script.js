7ebc6034196d68429750f27a9aff956562554bf8
Shamble94 <fratus.nicholas@gmail.com>
2023-11-23 14:20:52 +0100

    [UPDATE] fixed error in cycle. Esercizio finito

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,15 +1,30 @@
+/* DICHIARO ARRAY CON EMAIL APPROVATE */
 const mail_approvate = ["pippo@gmail.com", "pappo@gmail.com", "ciccio@gmail.com", "carlo@gmail.com" ]
+
+/* DICHIARO VARIABILE APPROVAZIONE */
+let approvazione = false;
+
+/* SALVO ARRAY IN CONSOLE */
 console.log(mail_approvate)
+
+/* DICHIARO VARIABILE EMAIL INSERITA DALL'UTENTE TRAMITE PROMPT */
 let mail_utente = prompt ("Inserisci la tua email")
+
+/* SALVO L'EMAIL UTENTE IN CONSOLE */
 console.log(mail_utente)
 
-
+/* CICLO FOR PER VERIFICARE LA PRESENZA DELL'EMAIL UTENTE ALL'INTERNO DELL'ARRAY */
 for (let i=0; i < mail_approvate.length; i++){
-    if (mail_approvate[i] === mail_utente){
-        alert("Complimenti! La sua email corrisponde ad una email nella nostra lista e quindi può entrare!")
-        break
-    } else{
-        alert("Siamo spiacenti, la sua email non risulta tra la nostra lista di email approvate e quindi non può entrare")
-        break
+    /* SE L'EMAIL INSERITA CORRISPONDE, LA VARIABILE APPROVAZIONE ASSUMERA IL VALORE TRUE */
+    if (mail_approvate[i] == mail_utente){
+        approvazione = true
     }
+}
+
+if (approvazione == true){
+    /* SE LA VARIABILE APPROVAZIONE HA VALORE TRUE VERRA VISUALIZZATO QUESTO ALERT */
+    alert("Complimenti la sua email risulta presente nella nostra lista! Puo entrare!")
+}else{
+    /* SE LA VARIABILE APPROVAZIONE HA VALORE FLASE VERRA VISUALIZATO QUESTO */
+    alert("Siamo spiacenti, la sua email non risulta presente nella nostra lista, non puo entrare")
 }
